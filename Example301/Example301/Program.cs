8d06fa7e53f7ad0f6cbfddb17cbbbdbe3b9fd5c1
rushmi0 <gcalku@gmail.com>
2024-07-06 09:03:10 +0700

    first commit

---
+++ Example301/Example301/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Example301
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input a number : ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if (num > 0)
+            {
+                Console.WriteLine("This is positive number.");
+            }
+
+            Console.WriteLine("You input number = {0}", num);
+            Console.ReadKey();
+        }
+    }
+}
