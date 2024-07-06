9e9ed896ca2e1e149b8a0c55a205315da3c6d44b
rushmi0 <gcalku@gmail.com>
2024-07-06 09:06:22 +0700

    first commit

---
+++ Lab1105/Lab1105/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lab1105
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] number = new int[5];
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.Write("Input number {0} : ", i+1);
+                number[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            ShowEven(number);
+            ShowOdd(number);
+            Console.ReadKey();
+        }
+
+        static void ShowEven(int[] num)
+        {
+            Console.WriteLine("List of Even number");
+            Console.WriteLine("---------------------------------");
+            foreach (int _num in num)
+            {
+                if (_num % 2 == 0)
+                {
+                    Console.Write("{0}", _num);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static void ShowOdd(int[] num)
+        {
+            Console.WriteLine();
+            Console.WriteLine("List of Odd number");
+            Console.WriteLine("--------------------------------");
+            foreach (int _num in num)
+            {
+                if (_num % 2 != 0)
+                {
+                    Console.Write("{0}", _num);
+                }
+            }
+
+        }
+    }
+}
---
+++ Lab1106/Lab1106/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab1106
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Number of advisee is {0}", CountStudent());
+            Console.WriteLine("-------------------------");
+            ShowStudent();
+            Console.ReadKey();
+
+        }
+
+        static int CountStudent()
+        {
+            string filename = "D:\\2013111089\\Advisee.csv";
+            StreamReader reader = new StreamReader(filename);
+            string temp = "";
+            int count = 0;
+            while ((temp = reader.ReadLine()) != null)
+            {
+                count++;
+            }
+            reader.Close();
+            return count;
+        }
+
+        static void ShowStudent()
+        {
+            string filename = "D:\\2013111089\\Advisee.csv";
+            StreamReader reader = new StreamReader(filename);
+            string temp = "";
+            while ((temp = reader.ReadLine()) != null)
+            {
+                string[] data = temp.Split(',');
+                Console.WriteLine("{0}\t{1}", data[0], data[3]);
+            }
+            reader.Close();
+        }
+    }
+}
