f281dfece7964743aa47cf016202b040f77f16c7
rushmi0 <gcalku@gmail.com>
2024-07-06 09:03:20 +0700

    first commit

---
+++ Example302/Example302/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Example302
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input number : ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Input number : ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            //Console.WriteLine("Both of number {0} equal.", num1 == num2 ? "are" : "are NOT");
+
+            if (num1 == num2)
+            {
+                Console.WriteLine("Both of number are equal.");
+            }
+            else
+            {
+                Console.WriteLine("Both of number are NOT equal.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
