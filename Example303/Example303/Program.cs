a6b594c41a12d6c15852d5f23039041fdc34ca95
rushmi0 <gcalku@gmail.com>
2024-07-06 09:03:29 +0700

    first commit

---
+++ Example303/Example303/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Example303
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input a number : ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (num > 0)
+            {
+                Console.WriteLine("{0} is positive number.", num);
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("{0} is negative number.", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is zero number.", num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
