208db457117ac25da7e03eb652b7621b96ef9865
rushmi0 <gcalku@gmail.com>
2024-07-06 09:03:49 +0700

    first commit

---
+++ Example401/Example402/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Example402
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N;
+
+            do
+            {
+
+                Console.Write("Input value of N : ");
+                N = int.Parse(Console.ReadLine());
+
+            }
+            while (N <= 1);
+
+            int sum = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Summary of 1 - {0} is {1:N}", N, sum);
+            Console.ReadKey();
+        }
+    }
+}
